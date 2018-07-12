4518a90a58f64e08e49706f2c9dfd1b692233a19
Spencer Hawkins <hawkins.spencer@gmail.com>
2018-07-12 11:58:36 -0700

    add ANA

--- source/mtga/set_data/__init__.py
+++ source/mtga/set_data/__init__.py
@@ -1,7 +1,7 @@
 from mtga.models.card_set import Pool
-from mtga.set_data import xln, w17, akh, hou, dom, aer, weird, kld, rix, m19
+from mtga.set_data import xln, w17, akh, hou, dom, aer, weird, kld, rix, m19, ana
 
 all_mtga_cards = Pool.from_sets("mtga_cards",
                                 sets=[rix.RivalsOfIxalan, xln.Ixalan, hou.HourOfDevastation, akh.Amonkhet, dom.Dominaria,
                                       kld.Kaladesh, aer.AetherRevolt, w17.WelcomeDecks2017, m19.CoreSet2019,
-                                      weird.WeirdLands])
+                                      ana.ArenaExlusives, weird.WeirdLands])
