cd6dfe91a29838570fda8877a673be5c9f28baa6
Milan Kelesi <kelesi@gmail.com>
2019-07-29 16:07:22 +0200

    Fix typos

--- source/mtga/set_data/__init__.py
+++ source/mtga/set_data/__init__.py
@@ -1,10 +1,9 @@
 from mtga.models.card_set import Pool
 from mtga.set_data import xln, dom, rix, m19, ana, grn, rna, war, m20
 
-<<<<<<< HEAD
 all_mtga_abilities = {}
 for set_abilities in (rix.set_ability_map, xln.set_ability_map, dom.set_ability_map, m19.set_ability_map,
-                      ana.set_ability_map, grn.set_ability_map, rna.set_ability_map, war.set_ability_map
+                      ana.set_ability_map, grn.set_ability_map, rna.set_ability_map, war.set_ability_map,
                       m20.set_ability_map):
     all_mtga_abilities.update(set_abilities)
 
