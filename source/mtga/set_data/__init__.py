4a3ee1815696b9ccfe4b4370846b242d141f5b59
Philip Kendall <philip@shadowmagic.org.uk>
2020-08-29 16:09:18 +0100

    Remove static data and throw if we can't find the Arena data files.
    The static data was very out of date and not useful any more, better to
    make this clear to any consumers than silently return unhelpful data.

--- source/mtga/set_data/__init__.py
+++ source/mtga/set_data/__init__.py
@@ -13,21 +13,4 @@ try:
                                         sets=[*dynamic_sets],
                                         abilities=all_mtga_abilities)
 except:
-    print("WARNING! Could not dynamically generate card sets. Do you have Arena installed?")
-    from mtga.set_data import xln, dom, rix, m19, ana, grn, rna, war, m20, eld, akh, arenasup, bfz, mi, roe, rtr
-
-    all_mtga_abilities = {**rix.set_ability_map, **xln.set_ability_map, **dom.set_ability_map, **m19.set_ability_map,
-                          **ana.set_ability_map, **grn.set_ability_map, **rna.set_ability_map, **war.set_ability_map,
-                          **m20.set_ability_map, **eld.set_ability_map, **akh.set_ability_map, **arenasup.set_ability_map,
-                          **bfz.set_ability_map, **mi.set_ability_map, **roe.set_ability_map, **rtr.set_ability_map}
-
-
-
-    all_mtga_cards = Pool.from_sets("mtga_cards",
-                                sets=[rix.RivalsOfIxalan, xln.Ixalan, dom.Dominaria, m19.CoreSet2019,
-                                      ana.ArenaExclusives, grn.GuildsOfRavnica, rna.RavnicaAllegiance,
-                                      war.WarOfTheSpark, m20.CoreSet2020, eld.ThroneOfEldraine, akh.Amonkhet,
-                                      arenasup.ArenaSup, bfz.BattleForZendikar, mi.Mirage, roe.RiseOfEldrazi,
-                                      rtr.ReturnToRavnica],
-                                abilities=all_mtga_abilities)
-
+    raise Exception("FATAL ERROR: could not dynamically generate card sets. Arena must be installed.")
